5f04192c6d917ba68f0214222aaca9931d257edd
Ali-Odeh <ali.odeh.pss@gmail.com>
2025-12-03 21:05:18 +0200

    fix importing two files that don't exist in server.js

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,8 @@ import systemChatRouter from "./routes/systemChatRouter.js";
 import messageRouter from "./routes/messageRouter.js";
 import notificationRouter from "./routes/notificationRouter.js";
 import authRouter from "./routes/authRoutes.js";
-import legislationRouter from "./routes/legislationRoute.js";
-import newsRouter from "./routes/newsRoute.js";
+// import legislationRouter from "./routes/legislationRoute.js";
+// import newsRouter from "./routes/newsRoute.js";
 import { startNotificationScheduler } from "./services/notificationScheduler.js";
 
 
@@ -36,11 +36,11 @@ app.use("/api/notifications", notificationRouter);
 // Route for Auth
 app.use("/api/auth", authRouter);
 
-// Route for Legislation
-app.use("/api/law", legislationRouter);
+// // Route for Legislation
+// app.use("/api/law", legislationRouter);
 
-// Route for News
-app.use("/api/news", newsRouter);
+// // Route for News
+// app.use("/api/news", newsRouter);
 
 app.get("/", (req, res) => {
     res.send("Justice-Connect Backend is running ✅");
